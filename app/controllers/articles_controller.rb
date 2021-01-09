fab3685fec3e6f257cf75f11f5b9034698d0ae4c
Sikor8711 <sikor87@hotmail.com>
2021-01-09 19:29:33 +0000

    New commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   def show
-    @article = Article.find(params[:id])
+
   end
 
   def index
@@ -12,7 +13,7 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+
   end
 
   def create
@@ -26,7 +27,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
+
     if @article.update(params.require(:article).permit(:title, :discription))
       flash[:notice] = 'Article was Edited succesfully'
       redirect_to @article
@@ -36,8 +37,15 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
+
     @article.destroy
     redirect_to articles_path
   end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
 end
